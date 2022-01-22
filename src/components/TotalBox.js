11d779cb46997f4726a1251a5eb0922f2ee023a4
navidabdi <artsanaat@gmail.com>
2022-01-22 11:48:48 +0330

    Format The Number And Camas

--- src/components/TotalBox.js
+++ src/components/TotalBox.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useStateContext } from '../contexts/StateCovidTracker';
-
+import { numberWithCommas } from '../Util';
 // Images
 import covidDefault from '../img/covid-defult.svg';
 import covidRed from '../img/covid-red.svg';
@@ -11,95 +11,109 @@ import covidOrange from '../img/covid-orange.svg';
 import covidRedDark from '../img/covid-redark.svg';
 
 const TotalBox = () => {
-  const { countryInfo } = useStateContext();
-
+  const { countryInfo, isLoading } = useStateContext();
+  const {
+    population,
+    cases,
+    deaths,
+    recovered,
+    active,
+    todayCases,
+    todayDeaths,
+    todayRecovered,
+    tests,
+  } = countryInfo;
   return (
-    <StyeldAllTotalBoxes>
-      <StyledTotalBox>
-        <div className="icon">
-          <img src={covidBlue} alt="" />
-        </div>
-        <div className="info">
-          <h3>Population</h3>
-          <h4>{countryInfo.population.NumberFormat('en-US')}</h4>
-        </div>
-      </StyledTotalBox>
-      <StyledTotalBox>
-        <div className="icon">
-          <img src={covidDefault} alt="" />
-        </div>
-        <div className="info">
-          <h3>Total Cases</h3>
-          <h4>{countryInfo.cases}</h4>
-        </div>
-      </StyledTotalBox>
-      <StyledTotalBox>
-        <div className="icon">
-          <img src={covidRed} alt="" />
-        </div>
-        <div className="info">
-          <h3>Total Deaths</h3>
-          <h4>{countryInfo.deaths}</h4>
-        </div>
-      </StyledTotalBox>
+    <>
+      {isLoading && (
+        <StyeldAllTotalBoxes>
+          <StyledTotalBox>
+            <div className="icon">
+              <img src={covidBlue} alt="" />
+            </div>
+            <div className="info">
+              <h3>Population</h3>
+              <h4>{numberWithCommas(population)}</h4>
+            </div>
+          </StyledTotalBox>
+          <StyledTotalBox>
+            <div className="icon">
+              <img src={covidDefault} alt="" />
+            </div>
+            <div className="info">
+              <h3>Total Tests</h3>
+              <h4>{numberWithCommas(tests)}</h4>
+            </div>
+          </StyledTotalBox>
+          <StyledTotalBox>
+            <div className="icon">
+              <img src={covidDefault} alt="" />
+            </div>
+            <div className="info">
+              <h3>Total Cases</h3>
+              <h4>{numberWithCommas(cases)}</h4>
+            </div>
+          </StyledTotalBox>
+          <StyledTotalBox>
+            <div className="icon">
+              <img src={covidRed} alt="" />
+            </div>
+            <div className="info">
+              <h3>Total Deaths</h3>
+              <h4>{numberWithCommas(deaths)}</h4>
+            </div>
+          </StyledTotalBox>
 
-      <StyledTotalBox>
-        <div className="icon">
-          <img src={covidGreen} alt="" />
-        </div>
-        <div className="info">
-          <h3>Total Recovered</h3>
-          <h4>{countryInfo.recovered}</h4>
-        </div>
-      </StyledTotalBox>
+          <StyledTotalBox>
+            <div className="icon">
+              <img src={covidGreen} alt="" />
+            </div>
+            <div className="info">
+              <h3>Total Recovered</h3>
+              <h4>{numberWithCommas(recovered)}</h4>
+            </div>
+          </StyledTotalBox>
 
-      <StyledTotalBox>
-        <div className="icon">
-          <img src={covidBlue} alt="" />
-        </div>
-        <div className="info">
-          <h3>Total Active</h3>
-          <h4>{countryInfo.active}</h4>
-        </div>
-      </StyledTotalBox>
+          <StyledTotalBox>
+            <div className="icon">
+              <img src={covidBlue} alt="" />
+            </div>
+            <div className="info">
+              <h3>Total Active</h3>
+              <h4>{numberWithCommas(active)}</h4>
+            </div>
+          </StyledTotalBox>
 
-      <StyledTotalBox>
-        <div className="icon">
-          <img src={covidOrange} alt="" />
-        </div>
-        <div className="info">
-          <h3>Today Cases</h3>
-          <h4>{countryInfo.todayCases}</h4>
-        </div>
-      </StyledTotalBox>
-      <StyledTotalBox>
-        <div className="icon">
-          <img src={covidRedDark} alt="" />
-        </div>
-        <div className="info">
-          <h3>Today Deaths</h3>
-          <h4>{countryInfo.todayDeaths}</h4>
-        </div>
-      </StyledTotalBox>
-      <StyledTotalBox>
-        <div className="icon">
-          <img src={covidGreen} alt="" />
-        </div>
-        <div className="info">
-          <h3>Today Recovered</h3>
-          <h4>{countryInfo.todayRecovered}</h4>
-        </div>
-      </StyledTotalBox>
-      <StyledTotalBox>
-        <div className="icon">
-          <img src={covidDefault} alt="" />
-        </div>
-        <div className="info">
-          <h3>Total Tests</h3>
-          <h4>{countryInfo.tests}</h4>
-        </div>
-      </StyledTotalBox>
-    </StyeldAllTotalBoxes>
+          <StyledTotalBox>
+            <div className="icon">
+              <img src={covidOrange} alt="" />
+            </div>
+            <div className="info">
+              <h3>Today Cases</h3>
+              <h4>{numberWithCommas(todayCases)}</h4>
+            </div>
+          </StyledTotalBox>
+          <StyledTotalBox>
+            <div className="icon">
+              <img src={covidRedDark} alt="" />
+            </div>
+            <div className="info">
+              <h3>Today Deaths</h3>
+              <h4>{numberWithCommas(todayDeaths)}</h4>
+            </div>
+          </StyledTotalBox>
+          <StyledTotalBox>
+            <div className="icon">
+              <img src={covidGreen} alt="" />
+            </div>
+            <div className="info">
+              <h3>Today Recovered</h3>
+              <h4>{numberWithCommas(todayRecovered)}</h4>
+            </div>
+          </StyledTotalBox>
+        </StyeldAllTotalBoxes>
+      )}
+    </>
   );
 };
 
@@ -107,7 +121,7 @@ export default TotalBox;
 
 const StyeldAllTotalBoxes = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(400px, 1fr));
+  grid-template-columns: repeat(auto-fit, minmax(318px, 1fr));
   grid-gap: 2em;
   margin-bottom: 2rem;
 `;
